3fe0c4d1e4143bd9e669537defb0ff55bc1d78b2
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-02-02 15:28:50 +0100

    added a simple vpc

---
+++ vpc.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "VPC-MRO"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "Public Subnet-MRO"
+  }
+}
+
+resource "aws_internet_gateway" "my_vpc_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "VPC-MRO - Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "my_vpc_eu_central_1a_public" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_vpc_igw.id
+  }
+  tags = {
+    Name = "MRO Public Subnet Route Table."
+  }
+}
+
+resource "aws_route_table_association" "my_vpc_eu_central_1a_public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.my_vpc_eu_central_1a_public.id
+}
